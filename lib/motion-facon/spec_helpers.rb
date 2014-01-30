d660679210a9f859c10a57f582cf9484b203e798
Spencer Steffen <spencer@citrusme.com>
2014-01-29 16:21:35 -0800

    only include facon modules once to prevent undefined method error when should extensions tries to undef methods

--- lib/motion-facon/spec_helpers.rb
+++ lib/motion-facon/spec_helpers.rb
@@ -1,6 +1,7 @@
 module Facon
   module SpecHelpers
     def self.extended(base)
+      return if Object.included_modules.include?(Facon::Mockable)
       Object.class_eval { include Facon::Mockable }
       base.class.class_eval { include Facon::Baconize::ContextExtensions }
       Should.class_eval { include Facon::Baconize::ShouldExtensions }
